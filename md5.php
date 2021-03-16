de05c2431bd790bec79f2a55123591ce0a425cea
NguyenDangToan <Toandeeng@gmail.com>
2021-03-16 23:37:13 +0700

    Update md5.php

--- md5.php
+++ md5.php
@@ -1,13 +1,15 @@
 <?php
     session_start();
     include 'logging.php';
+    //echo $_SESSION['path'];
     if(isset($_GET['file'])){
         $a = md5_file($_GET['file']);
-        echo 'MD5 '.$_GET['file'];
+        echo '<b>MD5 '.$_GET['file'].':</b>';
         echo '<br>'.$a;
     }
     else{
         $a = md5_file($_SESSION['path']);
-        echo 'MD5 '.$_SESSION['path'];
+        echo '<b>MD5 '.$_SESSION['path'].':</b>';
         echo '<br>'.$a;
     }
+?>
