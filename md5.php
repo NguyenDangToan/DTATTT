aba5592dc64e348cc5719c159d95b28e30f8fccd
NguyenDangToan <Toandeeng@gmail.com>
2021-03-18 21:29:55 +0700

    Update md5.php

--- md5.php
+++ md5.php
@@ -3,13 +3,19 @@
     include 'logging.php';
     //echo $_SESSION['path'];
     if(isset($_GET['file'])){
-        $a = md5_file($_GET['file']);
-        echo '<b>MD5 '.$_GET['file'].':</b>';
+        $file = str_replace('<','',$_GET['file'],$i);
+        if ($i){
+                die();
+        }
+        $a = md5_file($file);
+        echo '<b>MD5 '.$file.':</b>';
         echo '<br>'.$a;
+        echo "<br><a href='./index.html'>Back</a>";
     }
     else{
         $a = md5_file($_SESSION['path']);
         echo '<b>MD5 '.$_SESSION['path'].':</b>';
         echo '<br>'.$a;
+        echo "<br><a href='./index.html'>Back</a>";
     }
 ?>
